b1ec7f4b397403361394ca5de7f523d35cb21703
Bhishman Desai <bpdatal5@gmail.com>
2025-04-21 23:05:54 +0000

    Deployed `tts_model`

---
+++ deployments/tts_model/source.py
@@ -0,0 +1,26 @@
+import modelbit, sys
+from TTS.utils.synthesizer import Synthesizer
+import torch
+import io
+import torchaudio
+
+synthesizer = modelbit.load_value("data/synthesizer.pkl") # Synthesizer( (tts_model): Tacotron2( (embedding): Embedding(67, 512, padding_idx=0) (encoder): Encoder( (convolutions): ModuleList( (0-2): 3 x ConvBNBlock( (convolution1d): Conv1d(512, 512, kernel_siz...
+
+# main function
+def generate_speech(text: str) -> bytes:
+    with torch.no_grad():
+        # Generate audio using synthesizer
+        wav = synthesizer.tts(text)
+
+        # Convert numpy array to tensor
+        wav_tensor = torch.FloatTensor(wav).unsqueeze(0)
+
+        # Convert audio to bytes
+        buffer = io.BytesIO()
+        torchaudio.save(buffer, wav_tensor, sample_rate=22050, format="wav")
+        return buffer.getvalue()
+
+# to run locally via git & terminal, uncomment the following lines
+# if __name__ == "__main__":
+#   result = generate_speech(...)
+#   print(result)
