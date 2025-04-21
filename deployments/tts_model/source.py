1f1c8fff6d450eb1d287684418968f18b5db9603
Bhishman Desai <bpdatal5@gmail.com>
2025-04-21 23:57:42 +0000

    Deployed `tts_model`

--- deployments/tts_model/source.py
+++ deployments/tts_model/source.py
@@ -3,12 +3,12 @@ from TTS.utils.synthesizer import Synthesizer
 import torch
 import io
 import torchaudio
-import flask
+import base64
 
 synthesizer = modelbit.load_value("data/synthesizer.pkl") # Synthesizer( (tts_model): Tacotron2( (embedding): Embedding(67, 512, padding_idx=0) (encoder): Encoder( (convolutions): ModuleList( (0-2): 3 x ConvBNBlock( (convolution1d): Conv1d(512, 512, kernel_siz...
 
 # main function
-def generate_speech(text: str):
+def generate_speech(text: str) -> str:
     with torch.no_grad():
         # Generate audio using synthesizer
         wav = synthesizer.tts(text)
@@ -21,12 +21,10 @@ def generate_speech(text: str):
         torchaudio.save(buffer, wav_tensor, sample_rate=22050, format="wav")
         audio_bytes = buffer.getvalue()
         
-        # Create a Flask response with the appropriate content type
-        response = flask.Response(audio_bytes)
-        response.headers['Content-Type'] = 'audio/wav'
-        response.headers['Content-Disposition'] = 'attachment; filename=speech.wav'
+        # Convert bytes to base64 string
+        base64_audio = base64.b64encode(audio_bytes).decode('utf-8')
         
-        return response
+        return base64_audio
 
 # to run locally via git & terminal, uncomment the following lines
 # if __name__ == "__main__":
